b7cdd66924022766d00983641c0a1c40998fb74d
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-18 22:46:05 +0700

    Config URL

--- HTM_Season2/chat/views.py
+++ HTM_Season2/chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def chat_box(request, chat_box_name):
+    return render(request, 'chat/chat_box.html', {"chat_box_name": chat_box_name})
