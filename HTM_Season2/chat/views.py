a941c09cc6f7dfebdd11e384ea0db046ec648c9d
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-18 23:16:45 +0700

    Add template

--- HTM_Season2/chat/views.py
+++ HTM_Season2/chat/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 
 def chat_box(request, chat_box_name):
-    return render(request, 'chat/chat_box.html', {"chat_box_name": chat_box_name})
+    return render(
+        request,
+        'chat/chat_box.html',
+        {"chat_box_name": chat_box_name}
+    )
