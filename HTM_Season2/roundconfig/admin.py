f82fc0bb9f521094a373d6c8a24dcd0671232529
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-23 10:26:20 +0700

    Add model to admin

--- HTM_Season2/roundconfig/admin.py
+++ HTM_Season2/roundconfig/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import QuestionSetConfig
+from .models import QuestionConfig
 
 # Register your models here.
 admin.site.register(QuestionSetConfig)
+admin.site.register(QuestionConfig)
