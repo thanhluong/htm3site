9f6135c5323c4c803264fd5b9da33fcecb9046b9
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-23 10:41:13 +0700

    Finish CurrentQuestion model

--- HTM_Season2/roundconfig/models.py
+++ HTM_Season2/roundconfig/models.py
@@ -24,6 +24,13 @@ class QuestionSetConfig(models.Model):
 
 
 class QuestionConfig(models.Model):
+    FILE_TYPE_CHOICES = (
+        ("None", "Không có file"),
+        ("audio", "Âm thanh"),
+        ("video", "Video"),
+        ("image", "Hình ảnh")
+    )
+
     currentQuestionID = models.IntegerField(
         default=0,
         verbose_name="ID câu hỏi hiện tại"
@@ -31,4 +38,15 @@ class QuestionConfig(models.Model):
     currentQuestionContent = models.TextField(
         default="",
         verbose_name="Nội dung câu hỏi hiện tại"
+    )
+    currentQuestionFileType = models.CharField(
+        max_length=16,
+        choices=FILE_TYPE_CHOICES,
+        default="None",
+        verbose_name="Loại file đính kèm"
+    )
+    currentQuestionFile = models.TextField(
+        default="",
+        blank=True,
+        verbose_name="Đường dẫn đính kèm"
     )
