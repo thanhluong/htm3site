589bd73dcdd501b4fbc0c87415fefce27d991b4b
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-24 16:20:36 +0700

    Add RoundState to Admin

--- HTM_Season2/roundconfig/admin.py
+++ HTM_Season2/roundconfig/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import QuestionSetConfig
 from .models import QuestionConfig
+from .models import RoundState
 
 # Register your models here.
 admin.site.register(QuestionSetConfig)
 admin.site.register(QuestionConfig)
+admin.site.register(RoundState)
