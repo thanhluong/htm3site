f45d779e80ea8a206155096cd47b24fe762632ba
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-24 20:28:26 +0700

    Add GameState to Admin

--- HTM_Season2/roundconfig/admin.py
+++ HTM_Season2/roundconfig/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import QuestionSetConfig
 from .models import QuestionConfig
 from .models import RoundState
+from .models import GameState
 
 # Register your models here.
 admin.site.register(QuestionSetConfig)
 admin.site.register(QuestionConfig)
 admin.site.register(RoundState)
+admin.site.register(GameState)
