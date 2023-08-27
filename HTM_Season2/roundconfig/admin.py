f50830bf4394b6df48ac19e62d90d70b630a6820
thanhluong <doannguyenthanhluong@gmail.com>
2023-08-27 08:57:54 +0700

    Update Admin site

--- HTM_Season2/roundconfig/admin.py
+++ HTM_Season2/roundconfig/admin.py
@@ -4,8 +4,13 @@ from .models import QuestionConfig
 from .models import RoundState
 from .models import GameState
 
+
+class RoundStateAdmin(admin.ModelAdmin):
+    filter_horizontal = ('allRingers', 'allNSHVers',)
+
+
 # Register your models here.
 admin.site.register(QuestionSetConfig)
 admin.site.register(QuestionConfig)
-admin.site.register(RoundState)
+admin.site.register(RoundState, RoundStateAdmin)
 admin.site.register(GameState)
