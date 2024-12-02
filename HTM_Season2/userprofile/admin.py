490a6905d3937e74dff6d45ad79f7378ebd0a518
thanhluong <doannguyenthanhluong@gmail.com>
2024-12-03 00:12:05 +0700

    Add readonly_fields

--- HTM_Season2/userprofile/admin.py
+++ HTM_Season2/userprofile/admin.py
@@ -5,6 +5,7 @@ from .forms import CustomUserCreationForm
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "is_superuser", "is_staff")
     list_filter = ("is_superuser", "is_staff", "is_active")
+    readonly_fields = ('date_joined', 'last_login')
 
     fieldsets = [
         ('Thông tin cá nhân', {'fields': ['first_name', 'last_name', 'email']}),
