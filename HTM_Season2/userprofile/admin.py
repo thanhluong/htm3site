4b78c5010bfc2e9f09e97544dfd495cc392f943d
thanhluong <doannguyenthanhluong@gmail.com>
2024-12-14 16:43:15 +0700

    Update MyUser admin

--- HTM_Season2/userprofile/admin.py
+++ HTM_Season2/userprofile/admin.py
@@ -7,9 +7,10 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ("is_superuser", "is_staff", "is_active")
 
     fieldsets = [
-        ('Thông tin cá nhân', {'fields': ['first_name', 'last_name', 'email']}),
+        ('Thông tin cá nhân', {'fields': ['first_name', 'last_name', 'display_name', 'email']}),
         ('Thông tin đăng nhập', {'fields': ['username', 'password']}),
         ('Thông tin quyền hạn', {'fields': ['is_superuser', 'is_staff', 'is_active']}),
+        ('Thông tin khác', {'fields': ['profile_pic', 'is_contestant']}),
     ]
 
 # Register your models here.
